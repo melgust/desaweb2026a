f7752cbbcff1f183d0b180f3ee8227205b5fb186
javier6441112 <jsandovalp9@miumg.edu.gt>
2026-08-29 08:21:27 -0600

    adicion de objetos para realizar merge

--- practice3/mcalic1/backend/src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ practice3/mcalic1/backend/src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c371e3ec37334b328e69072d46cc887bcd98c7b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f560fb88fb19ab59e6eb60fb8dd84fc5df60abd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
