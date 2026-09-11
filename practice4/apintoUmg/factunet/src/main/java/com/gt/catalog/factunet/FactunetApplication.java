ce8e852a1c6066c328192864b8615c92ac965ccd
Angelina_PG <angelinapinto1278r@gmail.com>
2026-09-11 12:16:55 -0600

    Laboratorio 2
    Creación de proyecto para API con sprig boot

---
+++ practice4/apintoUmg/factunet/src/main/java/com/gt/catalog/factunet/FactunetApplication.java
@@ -0,0 +1,13 @@
+package com.gt.catalog.factunet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactunetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactunetApplication.class, args);
+	}
+
+}
---
+++ practice4/apintoUmg/factunet/src/test/java/com/gt/catalog/factunet/FactunetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gt.catalog.factunet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactunetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
